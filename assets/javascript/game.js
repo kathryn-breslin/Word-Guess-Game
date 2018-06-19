0aa65b529a2986178dc4c7a8f0e23e1e46e85fcb
Kat Bres <kathryn.l.breslin@gmail.com>
2018-06-19 18:11:15 -0500

    from scratch

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,108 @@
-var bikeWords = ["tire", "handles", "spokes", "brakes", "pedals"];
+var bikeWords = [ //array from which the random bike word will be chosen
+    "handles",
+    "spokes",
+    "tires",
+    "brakes",
+    "pedals",
+    "frame"
+];
 
-//this will choose a random word
-var bikeWord = bikeWords[Math.floor(Math.random() * bikeWords.length)];
 
-//this is the for loop that will walk through the answer
-var randomBikeWord = [];
-for (var i = 0; i < bikeWord.length; i++) {
-    randomBikeWord[i] = "_";
-}
+const totalNumberOfGuesses = 10; //a 'const' is a block-scope, the value cannot change or be redeclared
 
-var remainingLetters = bikeWord.length;
 
-var wins=0;
-var losses=0;
-var guesses=10;
-//created variables to be used. 
+var lettersGuessed = []; //array to hold the guessed letters
+var currentWord;
+var bikeWord = []; //The random bike word to be guessed; is an array to be filled with correct letters
+var remainingGuesses = 0;
+var gameHasBegun = false;
+var gameHasEnded = false;
+var wins = 0;
 
-//a while loop allows the action to repeat itsef until the answer is no longer true.
-while (remainingLetters > 0) {
-document.write(randomBikeWord.join(" "));
+//Global variables above// 
 
-var userGuess = document.getElementById("keyPress"); 
-document.onkeyup = function(event) {
-userGuess.textContent = event.key;
-}   
+function beginGame() {
+    remainingGuesses = totalNumberOfGuesses;
+    gameHasBegun = false;
+    currentWord = Math.floor(Math.random() * (bikeWords.length));
 
-for (var g = 0; g < bikeWord.length; g++) {
-    if(bikeWord[g] === userGuess) {
-    randomBikeWord[g] = userGuess; 
-    remainingLetters--;
-    guesses--;
+    lettersGuessed = []; //both arrays to be used during the game
+    bikeWord = []; 
+
+    for (var i = 0; i < bikeWords[currentWord].length; i++) {
+    bikeWord.push("_ "); //for each individual letter the underscore will push to the html, depending on the random word that was chosen
     }
-}
-}
 
-document.write(randomBikeWord.join(" "));
-document.write("Awesome! The bike word was " + bikeWord);
+    updateDisplay();
+};
 
-//Begin with press any key to start
-//define words that are to be guessed
+function updateDisplay() {
 
-//on the screen, have a _ _ _ _ _ _ where the word is supposed to be <maybe use index numbers to help with the guessing part -- like when the letter is pressed, on the click, the index is related and then logs?
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("currentWord").innerHTML = "";
 
-//log each incorrect key that is pressed with a number that goes down from 10 - 1
+    for (var i = 0; i < bikeWord.length; i++) {
+        document.getElementById("currentWord").innerHTML+= bikeWord[i]; //'+=' is the same at x = x + y
+    }
+    document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+    document.getElementById("lettersGuessed").innerHTML = lettersGuessed; 
 
-//when correct letter is pressed, log that letter in the correct order of the word
+    if(document.getElementById("status") != null){ //added this to combat the null status
+        document.getElementById("status").innerHTML;
+    }
 
-//if the amount of attempts run out, alert the player with "You lost! Try again next time."
+    if(remainingGuesses <= 0) {
+        gameHasEnded = true;
+    }
+};
+
+document.onkeyup = function(event) { //getting the game to begin (again)
+    if(gameHasEnded) {
+        beginGame()
+        gameHasEnded = false;
+    }
+    else {
+        if (event.keyCode >= 65 && event.keyCode <= 90) { //only the letters keys will be enables since their keycode is between 65 & 90
+            makeGuess(event.key.toLowerCase()); //will return uppercase letters to lowercase submissions
+        }
+    }
+};
+
+function makeGuess(letter) {
+    if (remainingGuesses > 0) {
+        if(!gameHasBegun) {
+            gameHasBegun = true;
+        }
+    if (lettersGuessed.indexOf(letter) === -1) {
+        lettersGuessed.push(letter);
+        evaluateGuess(letter);
+        }
+    }
+    updateDisplay();
+    checkWin();
+};
+
+function evaluateGuess(letter) {
+    var positions = []; //stores the positions of the letters
+    
+    for (var i = 0; i < bikeWords[currentWord].length; i++) {
+        if(bikeWords[currentWord][i] === letter) {
+            positions.push(i);
+        }
+    }
+    if (positions.length <= 0) {
+        remainingGuesses--;
+    }
+    else {
+        for (var i = 0; i < positions.length; i++) {
+            bikeWord[positions[i]] = letter;
+        }
+    }
+};
+
+function checkWin() {
+    if(bikeWord.indexOf("_") === -1) {
+        wins++;
+        gameHasEnded = true;
+    }
+};
